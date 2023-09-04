acab71029f5a6ad38a1687a3db9beb3c2bb5c7da
QishengHu <brandonhu0215@yahoo.com>
2023-09-04 13:45:28 +0800

    upload

---
+++ app/components/HeadWeb.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+
+export const HeadWeb = () => {
+  return (
+    <Head>
+        <title>Messenger</title>
+        <meta name="description" content="Clone Messenger" />
+        <link rel="icon" href="https://logodownload.org/wp-content/uploads/2017/04/facebook-messenger-logo-0-1.png" />
+    </Head>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import { Nunito } from 'next/font/google'
+
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
+
+import ToasterProvider from '@/app/providers/ToasterProvider';
+
+import './globals.css'
+import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
+import Head from 'next/head';
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+}
+
+const font = Nunito({ 
+  subsets: ['latin'], 
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      {/* <head>
+        <title>Airbnb</title>
+        <meta name="description" content="Airbnb Clone" />
+        <link rel="icon" href="https://th.bing.com/th/id/OIP.XVG1A3K8KsIWzckT_E6NRAHaHa?pid=ImgDet&w=558&h=558&rs=1" />
+      </head> */}
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
